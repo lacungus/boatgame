3e6e20e5937cb0e9dc42ba9851c2386d80f5035f
lacungus <lacungus@gmail.com>
2015-02-08 21:06:42 -0800

    boatgame: initial commit

---
+++ main.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	print("Ready!")
+	set_process(true)
+
+func _process(delta):
+	var ball = get_node("Ball")
+	
+	if (Input.is_action_pressed("ui_left")):
+		ball.apply_impulse( Vector2(-10, 0), Vector2(-10, 0) )
+
+	if (Input.is_action_pressed("ui_right")):
+		ball.apply_impulse( Vector2(10, 0), Vector2(10, 0) )
