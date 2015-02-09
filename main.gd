08a0e153bc9d0d90bbb0831f22b74d33881fa4b3
lacungus <lacungus@gmail.com>
2015-02-08 22:47:00 -0800

    Extact obtaining player objects to _ready

--- main.gd
+++ main.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
+var player
+var opponent
+
 func _ready():	
-	var player = get_node("player")
-	var opponent = get_node("opponent")
+	player = get_node("player")
+	opponent = get_node("opponent")
 	
 	PS2D.body_add_collision_exception(player.get_rid(), opponent.get_rid())
 	
 	set_process(true)
 
 func _process(delta):
-	var ball = get_node("player")
-	
 	if (Input.is_action_pressed("ui_left")):
-		ball.apply_impulse( Vector2(-10, 0), Vector2(-10, 0) )
+		player.apply_impulse( Vector2(-10, 0), Vector2(-10, 0) )
 
 	if (Input.is_action_pressed("ui_right")):
-		ball.apply_impulse( Vector2(10, 0), Vector2(10, 0) )
+		player.apply_impulse( Vector2(10, 0), Vector2(10, 0) )
