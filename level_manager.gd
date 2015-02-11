80ec0427b7e6f876ed45ab3e5cdda93b01c784ce
lacungus <lacungus@gmail.com>
2015-02-10 22:32:54 -0800

    First level: put the opponent lower

--- level_manager.gd
+++ level_manager.gd
@@ -9,7 +9,7 @@ func get_next_level():
 	count = count + 1
 	if count == 1:
 		var characters = [application.create_player(), application.create_swinging_opponent()]
-		var positions = [Vector2(100, 100), Vector2(200, 200)]
+		var positions = [Vector2(100, 100), Vector2(400, 400)]
 		return preload("level.gd").new(characters, positions)
 
 	if count == 2:
