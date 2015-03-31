83fedbf8d25294bbd6086e71d29fa60272bb35b7
lacungus <lacungus@gmail.com>
2015-03-30 20:23:28 -0700

    Show stars in levels menu.

--- src/ui/levels_menu/levels_menu.gd
+++ src/ui/levels_menu/levels_menu.gd
@@ -10,15 +10,21 @@ var application
 func _ready():
 	application = get_node("/root/application")
 	
-	for i in range(application.get_level_manager().get_level_count()):		
+	for i in range(application.get_level_manager().get_level_count()):
+		var stars = application.get_level_manager().get_stars_per_level(i + 1)
 		var button = Button.new()
 		button.set_pos(Vector2(get_button_start_x(), BUTTON_HEIGHT * i + INTERVAL * i))
 		button.set_size(Vector2(BUTTON_WIDTH, BUTTON_HEIGHT))
-		button.set_text("Level " + str(i + 1))
+		button.set_text("Level " + str(i + 1) + ". " + star_message(stars))
 		button.connect("pressed", self, "on_button_clicked", [i + 1])
 		get_node("levels_bar").add_child(button)
 
 	pass
+
+func star_message(stars):
+	if stars == null:
+		return "Not completed."
+	return str(stars) + " stars."
 
 func get_button_start_x():
 	return (get_node("levels_bar").get_rect().size.width - BUTTON_WIDTH) / 2
