124570082762e94f26e27fe568242c822e68f20d
lacungus <lacungus@gmail.com>
2015-04-14 21:23:03 -0700

    New pre level messages

--- src/pre_level.gd
+++ src/pre_level.gd
@@ -8,6 +8,8 @@ var messages
 
 var current_dialog_index = 0
 
+var current_dialog
+
 func _init(application, level, messages):
 	self.application = application
 	self.level = level
@@ -21,16 +23,29 @@ func show_dialogs():
 		level.start()
 		return
 		
-	var dialog = AcceptDialog.new()
+	var dialog_scene = preload("res://scenes/ui/pre_level_dialog.xml")
+	current_dialog = dialog_scene.instance()
+	current_dialog.get_node("label").set_text(messages[current_dialog_index])
+	var button = current_dialog.get_node("button")
+	button.set_text(get_button_text())
+	button.connect("pressed", self, "on_button_clicked")
+	
+	level.get_node("ui_layer").add_child(current_dialog)
+	center()
 
-	level.get_node("ui_layer").add_child(dialog)
-	dialog.set_size(Vector2(200, 100))
-	dialog.set_title("")
-	dialog.set_text(messages[current_dialog_index])
-	dialog.get_ok().set_text("Next")
-	dialog.get_ok().connect("pressed", self, "on_next_clicked")
-	dialog.popup_centered()
+func get_button_text():
+	if current_dialog_index == messages.size() - 1:
+		return "Start"
+	else:
+		return "Next"
 
-func on_next_clicked():
+func on_button_clicked():
 	current_dialog_index = current_dialog_index + 1
+	current_dialog.queue_free()
 	show_dialogs()
+
+func center():
+	var pos = current_dialog.get_pos()
+	pos.x = (application.get_width() - current_dialog.get_size().width) / 2
+	pos.y = (application.get_height() - current_dialog.get_size().height) / 2
+	current_dialog.set_pos(pos)
