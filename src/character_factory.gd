7f546c12bcbb9e8ab4bb2e5ea33bff04e10d9719
lacungus <lacungus@gmail.com>
2015-04-16 22:51:29 -0700

    Reorganize character factory

--- src/character_factory.gd
+++ src/character_factory.gd
@@ -1,21 +1,34 @@
 const PLAYER_MASS = 100
 const PLAYER_SPEED = 3
 
-const AI_CHAOTIC_MASS = PLAYER_MASS * 0.5
-const AI_CHAOTIC_SPEED = PLAYER_SPEED * 0.5
-
 const AI_SPEED = 2
-
 const AI_MASS = 10
 const AI_BALAN_MASS = 20
+
+# Chaotic = Ghost
+const AI_CHAOTIC_MASS = PLAYER_MASS * 0.5
+const AI_CHAOTIC_SPEED = PLAYER_SPEED * 0.5
+const AI_CHAOTIC_SPRITE_NAME = "ghost"
+
+# Following = Zombie
+const AI_FOLLOWING_MASS = PLAYER_MASS * 1
+const AI_FOLLOWING_SPEED = PLAYER_SPEED * 0.3
+const AI_FOLLOWING_SPRITE_NAME = "zombie"
+
+# Swinging = Hellhound
+const AI_SWINGING_MASS = PLAYER_MASS * 1
+const AI_SWINGING_SPEED = PLAYER_SPEED * 0.3
+const AI_SWINGING_SPRITE_NAME = "hellhound"
+
+# Evil = Demon
+const AI_EVIL_MASS = PLAYER_MASS * 1
+const AI_EVIL_SPEED = PLAYER_SPEED * 0.3
+const AI_EVIL_SPRITE_NAME = "demon"
+
 const PLAYER_SPRITE_NAME = "priest"
 const AI_SPRITE_NAME = "ghost"
 
-const GHOST_SPRITE_NAME = "ghost"
 const SKELETON_SPRITE_NAME = "skeleton"
-const DEMON_SPRITE_NAME = "demon"
-const ZOMBIE_SPRITE_NAME = "zombie"
-const HELLHOUND_SPRITE_NAME = "hellhound"
 
 var application
 
@@ -28,13 +41,13 @@ func create_player():
 	return create_character(preload("res://src/ai.gd").PlayerAI.new(application), PLAYER_SPEED, PLAYER_MASS, true, PLAYER_SPRITE_NAME)
 	
 func create_swinging_opponent():
-	return create_character(preload("res://src/ai.gd").SwingingAI.new(application), AI_SPEED, AI_MASS, false, HELLHOUND_SPRITE_NAME)
+	return create_character(preload("res://src/ai.gd").SwingingAI.new(application), AI_SPEED, AI_MASS, false, AI_SWINGING_SPRITE_NAME)
 
 func create_following_opponent():
-	return create_character(preload("res://src/ai.gd").FollowingAI.new(application), AI_SPEED, AI_MASS, false, ZOMBIE_SPRITE_NAME)
+	return create_character(preload("res://src/ai.gd").FollowingAI.new(application), AI_SPEED, AI_MASS, false, AI_FOLLOWING_SPRITE_NAME)
 
 func create_balancing_opponent():
-	return create_character(preload("res://src/ai.gd").BalancingAI.new(application), AI_SPEED, AI_BALAN_MASS, false, HELLHOUND_SPRITE_NAME)
+	return create_character(preload("res://src/ai.gd").BalancingAI.new(application), AI_SPEED, AI_BALAN_MASS, false, AI_SPRITE_NAME)
 	
 func create_opposing_opponent():
 	return create_character(preload("res://src/ai.gd").OpposingAI.new(application), AI_SPEED, AI_MASS, false, AI_SPRITE_NAME)
@@ -43,10 +56,10 @@ func create_sticking_opponent():
 	return create_character(preload("res://src/ai.gd").StickingAI.new(application), AI_SPEED, 1, false, AI_SPRITE_NAME)
 	
 func create_evil_opponent():
-	return create_character(preload("res://src/ai.gd").EvilAI.new(application), AI_SPEED, AI_MASS, false, DEMON_SPRITE_NAME)
+	return create_character(preload("res://src/ai.gd").EvilAI.new(application), AI_SPEED, AI_MASS, false, AI_EVIL_SPRITE_NAME)
 
 func create_chaotic_opponent():
-	return create_character(preload("res://src/ai.gd").ChaoticAI.new(application), AI_CHAOTIC_SPEED, AI_CHAOTIC_MASS, false, GHOST_SPRITE_NAME)
+	return create_character(preload("res://src/ai.gd").ChaoticAI.new(application), AI_CHAOTIC_SPEED, AI_CHAOTIC_MASS, false, AI_CHAOTIC_SPRITE_NAME)
 
 # PRIVATE 
 func create_character(ai, velocity, mass, is_player, sprite_name):
