2a438e6bfa6907feafb92d7b58d9a5cf73ffcc39
lacungus <lacungus@gmail.com>
2015-04-21 23:19:25 -0700

    Levels menu: button sizes

--- src/ui/levels_menu/levels_menu.gd
+++ src/ui/levels_menu/levels_menu.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-const BUTTON_WIDTH = 200
-const BUTTON_HEIGHT = 100
-
-const INTERVAL = 20
+const BUTTON_WIDTH = 500
+const BUTTON_HEIGHT = 50
 
 var application 
 
@@ -13,8 +11,7 @@ func _ready():
 	for i in range(application.get_level_manager().get_level_count()):
 		var stars = application.get_level_manager().get_stars_per_level(i + 1)
 		var button = Button.new()
-		button.set_pos(Vector2(get_button_start_x(), BUTTON_HEIGHT * i + INTERVAL * i))
-		button.set_size(Vector2(BUTTON_WIDTH, BUTTON_HEIGHT))
+		button.set_custom_minimum_size(Vector2(BUTTON_WIDTH, BUTTON_HEIGHT))
 		button.set_text("Level " + str(i + 1) + ". " + star_message(stars))
 		button.connect("pressed", self, "on_button_clicked", [i + 1])
 		get_node("levels_bar").add_child(button)
@@ -25,9 +22,6 @@ func star_message(stars):
 	if stars == null:
 		return "Not completed."
 	return str(stars) + " stars."
-
-func get_button_start_x():
-	return (get_node("levels_bar").get_rect().size.width - BUTTON_WIDTH) / 2
 
 func on_button_clicked(index):
 	application.get_scene_manager().goto_level(index)
