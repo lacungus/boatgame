6e43b9822ef34a84aa320d446636cfc879656306
lacungus <lacungus@gmail.com>
2015-05-12 21:17:10 -0700

    Resume: first draft

--- src/ui/in_level_menu/resume.gd
+++ src/ui/in_level_menu/resume.gd
@@ -1,7 +1,5 @@
-extends Control
-
 func _input_event(ev):
 	if (ev.type==InputEvent.MOUSE_BUTTON and ev.pressed):
 		var application = get_node("/root/application")
-		application.get_tree().set_pause(true)
-		application.get_node("/root/Game/ui_layer/in_level_menu").show()
+		application.get_tree().set_pause(false)
+		application.get_node("/root/Game/ui_layer/in_level_menu").hide()
