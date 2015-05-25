2044a2ea53abc8fd16d175674fcb390079ca94d4
lacungus <lacungus@gmail.com>
2015-05-24 19:53:19 -0700

    Move wind to config

--- src/level_manager.gd
+++ src/level_manager.gd
@@ -9,7 +9,7 @@ var start_y
 
 var stars_per_level 
 
-var level_config
+var levels_config
 
 const BASE_OFFSET = 10
 
@@ -52,8 +52,8 @@ func get_current_index():
 func parse_config():
 	var file = File.new()
 	file.open("res://config/levels.json", 1)
-	level_config = {}
-	var status = level_config.parse_json(file.get_as_text())
+	levels_config = {}
+	var status = levels_config.parse_json(file.get_as_text())
 	if status != OK:
 		print("Failed to parse levels config")
 		OS.get_main_loop().quit()
@@ -65,15 +65,17 @@ func get_level(index, set_current = false):
 	if set_current:
 		count = index
 		
-	var character_factory = application.get_character_factory() 
-	var wind = preload("res://src/wind.gd").new(application, 0.001, 0.0001)
+	var character_factory = application.get_character_factory()
+
+	var level_config = levels_config["levels"][index - 1]
+	print("Level config: " + str(level_config))
+	var wind = preload("res://src/wind.gd").new(application, level_config["wind"]["amplitude"], level_config["wind"]["period"])
 	
 	if index == 1:
 		# No wind
 		# One Chaotic Opponent
 		var characters = [character_factory.create_chaotic_opponent(), character_factory.create_player()]
 		var positions = start_positions(characters)
-		wind = preload("res://src/wind.gd").new(application, 0, 1)
 		var seconds_for_stars = [120, 60, 30]
 		var pre_level_messages = ["Look out! There is a ghost on your boat! You have to get rid of it!", "Push the buttons in the bottom to control your character. You goal is to force the enemy to fall off the board."]
 		return create_level(characters, positions, seconds_for_stars, wind, pre_level_messages)
@@ -83,7 +85,6 @@ func get_level(index, set_current = false):
 		# Two Chaotic Opponents
 		var characters = [character_factory.create_chaotic_opponent(), character_factory.create_chaotic_opponent(), character_factory.create_player()]
 		var positions = start_positions(characters)
-		wind = preload("res://src/wind.gd").new(application, 0, 1)
 		var seconds_for_stars = [120, 60, 30]
 		var pre_level_messages = ["Oh no! Two more ghosts are here! Quick, get rid of them!"]
 		return create_level(characters, positions, seconds_for_stars, wind, pre_level_messages)
@@ -93,7 +94,6 @@ func get_level(index, set_current = false):
 		# Four ghosts
 		var characters = [character_factory.create_chaotic_opponent(), character_factory.create_chaotic_opponent(), character_factory.create_chaotic_opponent(), character_factory.create_chaotic_opponent(), character_factory.create_player()]
 		var positions = start_positions(characters)
-		wind = preload("res://src/wind.gd").new(application, 0.0001, 0.0001)
 		var seconds_for_stars = [120, 60, 30]
 		var pre_level_messages = ["Too many ghosts! Luckily, fresh breeze is coming. Try using it for fighting your enemies."]
 		return create_level(characters, positions, seconds_for_stars, wind, pre_level_messages)
