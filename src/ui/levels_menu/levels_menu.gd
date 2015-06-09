3321d4b597e8e49f4afeff693bbaac08bb061820
Katya <kolikova@gmail.com>
2015-06-09 09:59:00 -0700

    Scrolling list of levels was added.

--- src/ui/levels_menu/levels_menu.gd
+++ src/ui/levels_menu/levels_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const BUTTON_WIDTH = 300
-const BUTTON_HEIGHT = 100
+const BUTTON_WIDTH = 150
+const BUTTON_HEIGHT = 150
 
 var application 
 
@@ -14,7 +14,8 @@ func _ready():
 		button.set_custom_minimum_size(Vector2(BUTTON_WIDTH, BUTTON_HEIGHT))
 		button.set_text("Level " + str(i + 1) + ". " + star_message(stars))
 		button.connect("pressed", self, "on_button_clicked", [i + 1])
-		get_node("levels_bar").add_child(button)
+		button.set_stop_mouse(true)
+		get_node("ScrollContainer/levels_bar").add_child(button)
 
 	pass
 
